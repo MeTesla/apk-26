9b776bd54b3c1c0877d73df87d5c0147cbb94fec
karim Mohamed <pookarim@gmail.com>
2025-10-17 22:58:40 +0100

    rien

--- client/components/misc/profile.js
+++ client/components/misc/profile.js
@@ -6,6 +6,7 @@ export function profile(){
     const div=document.createElement('div')
     div.className="user-profile"
     div.innerHTML =`
+        <div class="profile-container">
         <div class= "ligne"> NOM :</div> <h3>${objElv.nom}</h3>
         <div class= "ligne"> Prenom :</div> <h3>${objElv.prenom}</h3>
         <div class= "ligne"> Email :</div> <h3>${objElv.email}</h3>
@@ -13,24 +14,38 @@ export function profile(){
         <h2> Résultats</h2>
         <h4>QCM </h4>
         <div> ${JSON.parse(localStorage.getItem('resultatQcm')).score} </div> 
+        <div class="reset">Supprimer Profile</div>
         <style>
             .user-profile{
                 position: fixed;
                 top: 0; left: 0;
                 width:100%;
-                height: 100%;
+                height: 100%;                
+                background-color: white;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+            .profile-container{
+                width: 50%;
                 display: flex;
                 flex-direction: column;
                 align-items: center;
                 justify-content: center;
-                background-color: white;
             }
             .ligne{
                 width: 100%;
                 background-color: rgb(220,220,220);
                 padding: 10px;
+                border-radius: 5px;
             }
-        </style>`
+            .reset{
+                width: 160px;
+                padding:10px;
+                background-color: red;
+            }
+        </style>
+        </div>`
     document.body.style.overflow="hidden"
     document.body.appendChild(div)
 
@@ -38,5 +53,10 @@ export function profile(){
     userProfile.addEventListener('click', ()=>{
         document.body.style.overflow="auto"
         div.remove()
+    })
+
+    const reset = document.querySelector('.reset')
+    reset.addEventListener('click',()=>{
+        localStorage.clear()
     })
 }
--- client/main.js
+++ client/main.js
@@ -95,18 +95,17 @@ window.addEventListener("load", function () {
     const data = await reponse.json()
     if(data.token){
       localStorage.setItem('token', data.token)
-
-      // const {nom, prenom, email, tel, freeMins} = data.eleveUpdated
-      // console.log(nom, prenom, email, tel, freeMins);
-      // const objElv={nom, prenom, email, tel, freeMins}
-      // localStorage.setItem('profile', JSON.stringify(objElv))
+      console.log(data);
+      
+      const {nom, prenom, email, tel, freeMins} = data.eleveUpdated
+      console.log(nom, prenom, email, tel, freeMins);
+      const objElv={nom, prenom, email, tel, freeMins}
+      localStorage.setItem('profile', JSON.stringify(objElv))
 
       modalFreeMins(data.success, data.message, 'winner')
     } else{
       modalFreeMins(data.success, data.message)
     }
-    
-    console.log(data)
     
     //Appeler generate menu
     //modalCreerCompte()
