dbb2ed258ed8faa32efc9d25bd9bd42f31077576
karim Mohamed <pookarim@gmail.com>
2026-02-13 03:57:41 +0100

    add sendgrid api key

--- client/config/env.js
+++ client/config/env.js
@@ -1,8 +1,8 @@
 /**
  * Configuration d'environnement centralisée
  * À déploiement : modifier API_URL ou passer par variable d'environnement
- */
-
+ df*/ 
+ 
 // Déterminer l'URL de base selon l'environnement
 const isDevelopment = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
 const API_URL = 'https://apk-26-backend.onrender.com' //isDevelopment ? 'http://localhost:3000' : 'https://euduka.vercel.app'
