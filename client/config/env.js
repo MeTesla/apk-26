3bf98e30f944b5e44f97ef30cdce547360449408
karim Mohamed <pookarim@gmail.com>
2026-02-24 03:52:54 +0000

    verifier email CLIENT_URL

--- client/config/env.js
+++ client/config/env.js
@@ -1,10 +1,10 @@
 /**
  * Configuration d'environnement centralisée
  * À déploiement : modifier API_URL ou passer par variable d'environnement
- df*/ 
- 
+ df*/
+
 // Déterminer l'URL de base selon l'environnement
 const isDevelopment = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
 const API_URL = 'https://apk-26-backend.onrender.com' //isDevelopment ? 'http://localhost:3000' : 'https://euduka.vercel.app'
-
-export { API_URL }
+const CLIENT_URL = 'https://eudeka.onrender.com/'
+export { API_URL, CLIENT_URL }
